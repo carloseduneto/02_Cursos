35f7e5db5b1b0c1136aff43e5dde84451fb5cdba
Carlos Neto <112590775+carloseduneto@users.noreply.github.com>
2022-11-19 07:45:51 -0300

    Cursos

--- 00_FIAP_Python/Capitulo4_Dicionarios/Funcoes.py
+++ 00_FIAP_Python/Capitulo4_Dicionarios/Funcoes.py
@@ -1,6 +1,6 @@
 def menu():
     print("O que você deseja fazer?")
-    print("<I> Inserir \n<E> Excluir \n<L> Listar \n<S> Sair")
+    print("<I> Inserir \n<E> Excluir \n<L> Listar \n<P> Pesquisar \n<S> Sair")
 
 
 def insercao(dicionario):
@@ -8,3 +8,18 @@ def insercao(dicionario):
                                                input("Data de acesso: "),
                                                input("Local de acesso: ")]
     print(f"Adicionado com sucesso ✅\n")
+
+def pesquisar(dicionario):
+    chave=input("Digite a chave a ser pesquisada: ").upper()
+    if dicionario[chave]!="":
+        print(f"✅Usuário encontrado\n {dicionario[chave]}")
+    else:
+        print(f"❌Usuário inválido")
+def excluir(dicionario, chave):
+    chave = input("Digite a chave a ser excluída: ").upper()
+    del dicionario[chave]
+    print("✅ Usuário removido com sucesso!!!")
+
+def listar(dicionario):
+    for indice in dicionario:
+        print(indice)
--- 00_FIAP_Python/Capitulo4_Dicionarios/ManagerUsers.py
+++ 00_FIAP_Python/Capitulo4_Dicionarios/ManagerUsers.py
@@ -6,16 +6,26 @@ while opcaoEscolhida!="S":
     menu()
     opcaoEscolhida = input(">>").upper()
     if opcaoEscolhida=="I":
+        insercao(usuario)
+    if opcaoEscolhida== "E":
+        excluir(usuario)
+    if opcaoEscolhida=="L":
+        listar(usuario)
+    if opcaoEscolhida=="P":
+        pesquisar(usuario)
+
+
+
+
+
+
+
+
+
+
+
         #chave=input("Chave de acesso: ")
         #nome=input("Nome de cadastro: ")
         #dataDeAcesso=input("Data de acesso: ")
         #Localizacao=input("Local de acesso: ")
         #usuario[chave]=[nome,dataDeAcesso,Localizacao]
-        insercao(usuario)
-    if opcaoEscolhida== "E":
-        chave=input("Digite a chave a ser excluída: ")
-        del usuario [chave]
-        print("✅ Usuário removido com sucesso!!!")
-    if opcaoEscolhida=="L":
-        for i in usuario:
-            print(i)
